2a8d8ab53a5e680ea56e2304ea5896227568bb51
unknown <dmshastel@gmail.com>
2024-08-08 18:22:29 +0300

    homepage is added12345

--- homework-front-main/src/App.tsx
+++ homework-front-main/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import {HW1} from "./HW/1/HW1";
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-
-      <HW1 />
+      <h1>Hello world</h1>
+      {/*<HW1 />*/}
       {/*<HW2 />*/}
       {/*<HW3 />*/}
       {/*<HW4 />*/}
