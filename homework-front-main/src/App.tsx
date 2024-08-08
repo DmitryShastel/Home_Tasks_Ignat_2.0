4bc5c5e28171873a6414ba78648d36c8e744c7b5
unknown <dmshastel@gmail.com>
2024-08-08 21:36:32 +0300

    HW4 is done

--- homework-front-main/src/App.tsx
+++ homework-front-main/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import {HW3} from "./HW/3/HW3";
+import {HW4} from "./HW/4/HW4";
 
 export function App() {
     return (
         <div className="App">
             {/*<HW1/>*/}
             {/*<HW2 />*/}
-            <HW3 />
-            {/*<HW4 />*/}
+            {/*<HW3 />*/}
+            <HW4/>
         </div>
     );
 }
