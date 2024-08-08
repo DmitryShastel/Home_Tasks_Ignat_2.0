d6fac417434eb345c6847dd89ef6a668d159245a
unknown <dmshastel@gmail.com>
2024-08-08 17:01:51 +0300

    The project is created

---
+++ homework-front-main/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      {/*<HW1 />*/}
+      {/*<HW2 />*/}
+      {/*<HW3 />*/}
+      {/*<HW4 />*/}
+    </div>
+  );
+}
