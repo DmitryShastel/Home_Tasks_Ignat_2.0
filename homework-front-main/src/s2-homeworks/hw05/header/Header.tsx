6e3d6eed86de7ae7615d6e86cc9d6aa5ce377685
unknown <dmshastel@gmail.com>
2024-08-15 19:03:45 +0300

    HW2-5 is done

--- homework-front-main/src/s2-homeworks/hw05/header/Header.tsx
+++ homework-front-main/src/s2-homeworks/hw05/header/Header.tsx
@@ -3,24 +3,25 @@ import burgerIcon from './burger.svg'
 import s from './Header.module.css'
 // import {useLocation} from 'react-router-dom'
 import {PATH} from '../Pages'
+import {useLocation} from "react-router-dom";
 
 type PropsType = {
     handleOpen: () => void
 }
 
 export const Header: FC<PropsType> = ({handleOpen}) => {
-    // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
-    //const location = useLocation()
-    //const currentPath = location.pathname
+   // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
+    const location = useLocation()
+    const currentPath = location.pathname
 
-    // const pageName =
-    //     currentPath === PATH.PRE_JUNIOR
-    //         ? 'Pre-junior'
-    //         : currentPath === PATH.JUNIOR
-    //             ? 'Junior'
-    //             : currentPath === PATH.JUNIOR_PLUS
-    //                 ? 'Junior Plus'
-    //                 : 'Error'
+    const pageName =
+        currentPath === PATH.PRE_JUNIOR
+            ? 'Pre-junior'
+            : currentPath === PATH.JUNIOR
+                ? 'Junior'
+                : currentPath === PATH.JUNIOR_PLUS
+                    ? 'Junior Plus'
+                    : 'Error'
     return (
         <>
             <div id={'hw5-header'} className={s.header}>
