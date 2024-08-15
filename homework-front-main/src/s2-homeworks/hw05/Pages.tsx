98bdff0a3bbb28d1b60c7c8ef752c04d63a04d99
unknown <dmshastel@gmail.com>
2024-08-15 19:10:10 +0300

    HW2-5 is done

--- homework-front-main/src/s2-homeworks/hw05/Pages.tsx
+++ homework-front-main/src/s2-homeworks/hw05/Pages.tsx
@@ -16,15 +16,11 @@ function Pages() {
     return (
         <div>
             <Routes>
+                <Route path="/" element={null} />
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
                 <Route path='*' element={<Error404/>}/>
-
-                {/*<Route path={`#${PATH.PRE_JUNIOR}`} element={<PreJunior />} />*/}
-                {/*<Route path={`#${PATH.JUNIOR}`} element={<Junior />} />*/}
-                {/*<Route path={`#${PATH.JUNIOR_PLUS}`} element={<JuniorPlus />} />*/}
-                {/*<Route path='*' element={<Error404 />} />*/}
             </Routes>
         </div>
     )
