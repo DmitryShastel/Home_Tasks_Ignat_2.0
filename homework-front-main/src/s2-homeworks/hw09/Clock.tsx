7164bd782acb522422453cb82c862721a8f628a4
unknown <dmshastel@gmail.com>
2024-08-19 16:41:38 +0300

    HW-9 is done

--- homework-front-main/src/s2-homeworks/hw09/Clock.tsx
+++ homework-front-main/src/s2-homeworks/hw09/Clock.tsx
@@ -74,7 +74,7 @@ function Clock() {
 
 
     const stringTime = `${hours}:${minutes}:${seconds}`   //'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${day}:${month}:${year}`     //'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${day}.${month}.${year}`     //'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = dayOfWeek    //'date->day' || <br/> // пишут студенты
